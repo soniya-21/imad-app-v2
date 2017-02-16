95aef2e2d697092a075cee6e70f2029301304acd
soniya-21 <soniyasinghpanwar21@gmail.com>
2017-02-16 22:42:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //code to change the main-text div
 
 var element=document.getElementById('main-text');
-element.innerHTML='photo removed...:P';
+element.innerHTML='text changed...:P';
 
 //code to move the image
 var img=document.getElementById('madi');
