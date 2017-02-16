9892c2d6829289296b4650dfda2f732573033aec
soniya-21 <soniyasinghpanwar21@gmail.com>
2017-02-16 22:53:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHTML='text changed...:P';
 
 //code to move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+   // img.style.marginLeft='100px';
 };
