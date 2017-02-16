5d59067132cef5750c9f79c973ad5420403ac771
soniya-21 <soniyasinghpanwar21@gmail.com>
2017-02-16 23:02:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,11 @@ element.innerHTML='text changed...:P';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+    marginLeft = marginLeft + 1;//increase margin by 1 pixel
+    img.style.marginLeft = marginLeft + 'px';//concatenate 10 by px as both are strings
 }
 
 img.onclick=function(){
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,50);//move to right in every 50 ms
    // img.style.marginLeft='100px';
 };
